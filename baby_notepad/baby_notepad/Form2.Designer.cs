0c66ecd2f363010fc16a6ecf1aa71e7c0354bddc
anniezhen <43050334+anniezhen@users.noreply.github.com>
2019-07-05 05:42:42 -0400

    Revert "9"
    This reverts commit 84e64df580e35b7ccc92d2cd21f76d61a3fa6fc1.

--- baby_notepad/baby_notepad/Form2.Designer.cs
+++ baby_notepad/baby_notepad/Form2.Designer.cs
@@ -41,35 +41,30 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::baby_notepad.Properties.Resources.spaceglenda300;
-            this.pictureBox1.Location = new System.Drawing.Point(394, 174);
+            this.pictureBox1.Location = new System.Drawing.Point(357, 143);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(145, 189);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.Size = new System.Drawing.Size(214, 220);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.richTextBox1.Location = new System.Drawing.Point(28, 80);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
-            this.richTextBox1.Size = new System.Drawing.Size(372, 266);
+            this.richTextBox1.Size = new System.Drawing.Size(308, 266);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // textBox3
             // 
-            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox3.Location = new System.Drawing.Point(28, 43);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(372, 22);
+            this.textBox3.Size = new System.Drawing.Size(308, 29);
             this.textBox3.TabIndex = 2;
-            this.textBox3.Text = "Title\r\n";
+            this.textBox3.Text = "Title";
             this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
@@ -81,7 +76,7 @@
             this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightCoral;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.button1.Location = new System.Drawing.Point(431, 80);
+            this.button1.Location = new System.Drawing.Point(357, 100);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 28);
             this.button1.TabIndex = 3;
@@ -98,7 +93,7 @@
             this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Plum;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.Color.GhostWhite;
-            this.button2.Location = new System.Drawing.Point(431, 126);
+            this.button2.Location = new System.Drawing.Point(474, 100);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(83, 28);
             this.button2.TabIndex = 4;
@@ -115,7 +110,7 @@
             this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.ForeColor = System.Drawing.Color.GhostWhite;
-            this.button3.Location = new System.Drawing.Point(522, 307);
+            this.button3.Location = new System.Drawing.Point(525, 318);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(32, 28);
             this.button3.TabIndex = 5;
