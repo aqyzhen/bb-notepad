8f0708ba8084270b5f13d6510f486501de2d3a8e
anniezhen <43050334+anniezhen@users.noreply.github.com>
2019-07-05 05:45:31 -0400

    9

--- baby_notepad/baby_notepad/Form2.Designer.cs
+++ baby_notepad/baby_notepad/Form2.Designer.cs
@@ -41,28 +41,31 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::baby_notepad.Properties.Resources.spaceglenda300;
-            this.pictureBox1.Location = new System.Drawing.Point(357, 143);
+            this.pictureBox1.Location = new System.Drawing.Point(476, 176);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(214, 220);
+            this.pictureBox1.Size = new System.Drawing.Size(285, 271);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(28, 80);
+            this.richTextBox1.Location = new System.Drawing.Point(37, 98);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
-            this.richTextBox1.Size = new System.Drawing.Size(308, 266);
+            this.richTextBox1.Size = new System.Drawing.Size(409, 326);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(28, 43);
+            this.textBox3.Location = new System.Drawing.Point(37, 53);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(308, 29);
+            this.textBox3.Size = new System.Drawing.Size(409, 34);
             this.textBox3.TabIndex = 2;
             this.textBox3.Text = "Title";
             this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
@@ -76,11 +79,12 @@
             this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightCoral;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.button1.Location = new System.Drawing.Point(357, 100);
+            this.button1.Location = new System.Drawing.Point(476, 123);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 28);
+            this.button1.Size = new System.Drawing.Size(111, 34);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Save";
+            this.button1.Text = "Save as..";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -93,9 +97,10 @@
             this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Plum;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.Color.GhostWhite;
-            this.button2.Location = new System.Drawing.Point(474, 100);
+            this.button2.Location = new System.Drawing.Point(632, 123);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(83, 28);
+            this.button2.Size = new System.Drawing.Size(111, 34);
             this.button2.TabIndex = 4;
             this.button2.Text = "Delete";
             this.button2.UseVisualStyleBackColor = false;
@@ -110,9 +115,10 @@
             this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.ForeColor = System.Drawing.Color.GhostWhite;
-            this.button3.Location = new System.Drawing.Point(525, 318);
+            this.button3.Location = new System.Drawing.Point(700, 391);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(32, 28);
+            this.button3.Size = new System.Drawing.Size(43, 34);
             this.button3.TabIndex = 5;
             this.button3.Text = "←";
             this.button3.UseVisualStyleBackColor = false;
@@ -120,10 +126,10 @@
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Beige;
-            this.ClientSize = new System.Drawing.Size(593, 375);
+            this.ClientSize = new System.Drawing.Size(791, 462);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -131,6 +137,7 @@
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "New note";
